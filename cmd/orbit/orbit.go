692f16527151a89c9a5806c267ed775835497b82
Zach Latta <zach@zachlatta.com>
2015-03-01 01:44:14 +0000

    actually be able to create projects from the cli

--- cmd/orbit/orbit.go
+++ cmd/orbit/orbit.go
@@ -4,11 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"os/exec"
 
 	"github.com/zachlatta/orbit"
 	"gopkg.in/fsnotify.v1"
+)
+
+var (
+	baseURLStr = flag.String("url", "http://mew.hackedu.us:4000", "base URL of orbit")
+	baseURL    *url.URL
+
+	apiClient = orbit.NewClient(nil)
 )
 
 func init() {
@@ -26,7 +34,10 @@ The commands are:
 		}
 		fmt.Fprintln(os.Stderr, `
 Use "orbit command -h" for more information about a command.
+
+The options are:
 `)
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 }
@@ -38,6 +49,13 @@ func main() {
 		flag.Usage()
 	}
 	log.SetFlags(0)
+
+	var err error
+	baseURL, err = url.Parse(*baseURLStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	apiClient.BaseURL = baseURL.ResolveReference(&url.URL{Path: "/api/"})
 
 	subcmd := flag.Arg(0)
 	for _, c := range subcmds {
@@ -127,12 +145,30 @@ func commitAndPushEverything() error {
 }
 
 func createProjectCmd(args []string) {
-	client := orbit.NewClient(nil)
+	fs := flag.NewFlagSet("create-project", flag.ExitOnError)
+	fs.Usage = func() {
+		fmt.Fprintln(os.Stderr, `usage: orbit create-project [project name] [options]
+
+Create a new project on Orbit.
+`)
+		os.Exit(1)
+	}
+	fs.Parse(args)
+
+	if fs.NArg() != 1 {
+		fs.Usage()
+	}
+
+	projectName := fs.Args()[0]
 
 	var project orbit.Project
-	if err := client.Projects.Create(&project); err != nil {
+	if err := apiClient.Projects.Create(&project); err != nil {
+		log.Fatal(err)
+	}
+	cloneURL := baseURL.ResolveReference(&url.URL{Path: "/git/"}).ResolveReference(&url.URL{Path: project.GitPath})
+	if err := exec.Command("git", "clone", cloneURL.String(), projectName).Run(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(project)
+	fmt.Printf("%s created successfully\n", projectName)
 }
